c100100cf848e01897ea3f3a27de1350fac09902
Desh Iyer <desh.iyer@gmail.com>
2023-07-05 16:38:04 +0400

    🔨 Create function to load dataset

--- pre-process-music.py
+++ pre-process-music.py
@@ -1,3 +1,13 @@
+def loadSongsInKern(datasetPath):
+    '''
+    Function to recursively go through the passed dataset path and load the songs present using music21. Parameter is the path to the dataset.
+    '''
+    
+    pass
+
+    # Go through all files in the dataset directory and load
+
+
 def preprocess(datasetPath):
     '''
     Function to preprocess a dataset of music represented in .krn format. Parameter is the path to the dataset.
@@ -7,4 +17,10 @@ def preprocess(datasetPath):
 
     # Load the folk songs using music21
     
-    # Filter out songs with non-acceptable durations 
+    # Filter out songs with non-acceptable durations
+    
+    # Transpose songs to CMaj / AMin
+    
+    # Encode songs with musical time-series representation
+    
+    # Save songs to .txt file
