aeefff27e196eca67f5098143fd7174624b71c64
Desh Iyer <desh.iyer@gmail.com>
2023-07-05 16:33:56 +0400

    🔨 Create script to preprocess `.krn` data

---
+++ pre-process-music.py
@@ -0,0 +1,10 @@
+def preprocess(datasetPath):
+    '''
+    Function to preprocess a dataset of music represented in .krn format. Parameter is the path to the dataset.
+    '''
+    
+    pass
+
+    # Load the folk songs using music21
+    
+    # Filter out songs with non-acceptable durations 
