3a28dde37db6676757b768e1d1bc69dbde9fae74
Desh Iyer <desh.iyer@gmail.com>
2023-07-06 19:58:22 +0400

    🔨 Remove redundant characters

--- pre-process-music.py
+++ pre-process-music.py
@@ -2,7 +2,7 @@ def loadSongsInKern(datasetPath):
     '''
     Function to recursively go through the passed dataset path and load the songs present using music21. Parameter is the path to the dataset.
     '''
-    
+
     pass
 
     # Go through all files in the dataset directory and load
@@ -12,15 +12,15 @@ def preprocess(datasetPath):
     '''
     Function to preprocess a dataset of music represented in .krn format. Parameter is the path to the dataset.
     '''
-    
+
     pass
 
     # Load the folk songs using music21
-    
+
     # Filter out songs with non-acceptable durations
-    
+
     # Transpose songs to CMaj / AMin
-    
+
     # Encode songs with musical time-series representation
-    
+
     # Save songs to .txt file
