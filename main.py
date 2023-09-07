9924b3045951dcd8141f7147ffe182095ec01227
Desh Iyer <desh.iyer@gmail.com>
2023-09-07 19:40:41 +0530

    Organise imports

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-#DataFlair
 #load all the libraries
 from music21 import *
 import glob
 from tqdm import tqdm
 import numpy as np
 import random
-from tensorflow.keras.layers import LSTM,Dense,Input,Dropout
-from tensorflow.keras.models import Sequential,Model,load_model
+from tensorflow.keras.layers import LSTM,Dense, Input, Dropout
+from tensorflow.keras.models import Sequential, Model, load_model
 from sklearn.model_selection import train_test_split
 
 def read_files(file):
